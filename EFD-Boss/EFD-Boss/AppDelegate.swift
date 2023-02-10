2bf38422348d4cccd944905594b97795af2f1f4a
Elsa Firmin <elsafirmin@hotmail.fr>
2023-02-10 22:22:54 +0100

    Create Project Patron

---
+++ EFD-Boss/EFD-Boss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EFD-Boss
+//
+//  Created by Elsa on 10/02/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+
+
+}
---
+++ EFD-Boss/EFD-Boss/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  EFD-Boss
+//
+//  Created by Elsa on 10/02/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
