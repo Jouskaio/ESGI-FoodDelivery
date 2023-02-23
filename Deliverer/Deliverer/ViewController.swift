622978ebea43a419baec0ad4a7a2197e5d7a917c
tageldin <maaztageldin030@hotmail.com>
2023-02-23 01:33:33 +0100

    log in and signe up screen + language

--- Deliverer/Deliverer/ViewController.swift
+++ Deliverer/Deliverer/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(
+             self,
+             tableName: "Main",
+             bundle: .main,
+             value: self,
+             comment: self
+        )
+    }
 }
 
