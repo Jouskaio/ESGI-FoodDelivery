0284d6de05031c389192b9671a5b6078a92d51a3
Jouskaio <jouskaio.me@gmail.com>
2023-02-27 07:51:15 +0100

    Trying to correcting the zip message

--- Boss/EFD-Boss/EFD-Boss/DeliverersViewController.swift
+++ Boss/EFD-Boss/EFD-Boss/DeliverersViewController.swift
@@ -319,46 +319,28 @@ extension DeliverersViewController: UITableViewDataSource, CLLocationManagerDele
         let location: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
         let geocoder: CLGeocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
-            let pm = (placemarks ?? []) as [CLPlacemark]
-            if (!pm.isEmpty) {
-                self.zip = pm[0].postalCode!
-                self.city = pm[0].locality!
-                Logger.shared.log(.app, .info, "\(deliverer)")
+            let pm = (placemarks!) as [CLPlacemark]
+            self.zip = pm[0].postalCode!
+            self.city = pm[0].locality!
+            Logger.shared.log(.app, .info, "\(deliverer)")
 
-                deliverer.location_zip = self.zip
-                deliverer.location_city = self.city
-                if (error != nil) {
-                    print("reverse geodcode fail: \(error!.localizedDescription)")
-                }
-                //Set it in geocoder because it's a async request, if the text cell definition is in the tableview
-                // it will be in the main thread so this will display nothing
-                cell.textLabel?.numberOfLines = 0
-                cell.textLabel?.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)\n \(self.zip) \(self.city)"
-                // Update profil description with the last deliverer
-                self.nameText.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)"
-                self.mailText.text = "\(deliverer.deliverer_email)"
-                self.phoneText.text = "\(deliverer.deliverer_phone)"
-                self.zipText.text = "\(deliverer.location_zip)"
-                self.totalText.text = "\(deliverer.deliverer_total) €"
-                self.scoreText.text = "\(deliverer.deliverer_evaluation) / \(self.maxScore)"
-                self.scoreProgress.progress = deliverer.deliverer_evaluation / self.maxScore
-            } else {
-                deliverer.location_zip = "94470"
-                deliverer.location_city = "Boissy-Saint-Léger"
-                deliverer.location_latitude = 48.8655
-                deliverer.location_longitude = 2.3426
-
-                cell.textLabel?.numberOfLines = 0
-                cell.textLabel?.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)\n \(self.zip) \(self.city)"
-                // Update profil description with the last deliverer
-                self.nameText.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)"
-                self.mailText.text = "\(deliverer.deliverer_email)"
-                self.phoneText.text = "\(deliverer.deliverer_phone)"
-                self.zipText.text = "\(deliverer.location_zip)"
-                self.totalText.text = "\(deliverer.deliverer_total) €"
-                self.scoreText.text = "\(deliverer.deliverer_evaluation) / \(self.maxScore)"
-                self.scoreProgress.progress = deliverer.deliverer_evaluation / self.maxScore
+            deliverer.location_zip = pm[0].postalCode!
+            deliverer.location_city = pm[0].locality!
+            if (error != nil) {
+                print("reverse geodcode fail: \(error!.localizedDescription)")
             }
+            //Set it in geocoder because it's a async request, if the text cell definition is in the tableview
+            // it will be in the main thread so this will display nothing
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)\n \(self.zip) \(self.city)"
+            // Update profil description with the last deliverer
+            self.nameText.text = "\(deliverer.deliverer_firstname) \( deliverer.deliverer_name)"
+            self.mailText.text = "\(deliverer.deliverer_email)"
+            self.phoneText.text = "\(deliverer.deliverer_phone)"
+            self.zipText.text = "\(deliverer.location_zip)"
+            self.totalText.text = "\(deliverer.deliverer_total) €"
+            self.scoreText.text = "\(deliverer.deliverer_evaluation) / \(self.maxScore)"
+            self.scoreProgress.progress = deliverer.deliverer_evaluation / self.maxScore
         }
         return cell
     }
